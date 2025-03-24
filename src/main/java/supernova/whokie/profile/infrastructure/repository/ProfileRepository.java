7a959f297e5eae35c70463f25958e3904f89a812
momnpa333 <momnpa333@gmail.com>
2025-03-24 13:21:22 +0900

    feat: Profile 조회 시 N+1 문제 해결을 위한 fetch join

--- src/main/java/supernova/whokie/profile/infrastructure/repository/ProfileRepository.java
+++ src/main/java/supernova/whokie/profile/infrastructure/repository/ProfileRepository.java
@@ -1,11 +1,13 @@
 package supernova.whokie.profile.infrastructure.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import supernova.whokie.profile.Profile;
 
 import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
+    @Query("SELECT p FROM Profile p JOIN FETCH p.users u WHERE u.id = :userId")
     Optional<Profile> findByUsersId(Long userId);
 }
