32c486827bb098fec45a5ef481fa464c7af7b7e4
momnpa333 <momnpa333@gmail.com>
2025-03-31 10:48:58 +0900

    refact: visit Redis INCR 적용

--- src/main/java/supernova/whokie/redis/service/RedisVisitService.java
+++ src/main/java/supernova/whokie/redis/service/RedisVisitService.java
@@ -2,9 +2,13 @@ package supernova.whokie.redis.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
 import org.springframework.stereotype.Service;
 import supernova.whokie.global.annotation.RedissonLock;
 import supernova.whokie.profile.service.ProfileVisitReadService;
+import supernova.whokie.redis.constants.RedisConstants;
 import supernova.whokie.redis.entity.RedisVisitCount;
 import supernova.whokie.redis.entity.RedisVisitor;
 import supernova.whokie.redis.event.RedisDto;
@@ -25,20 +29,22 @@ public class RedisVisitService {
     private final RedisVisitorRepository redisVisitorRepository;
     private final RedisVisitCountRepository redisVisitCountRepository;
     private final ProfileVisitReadService profileVisitReadService;
+    private final RedissonClient redissonClient;
 
-    @RedissonLock(value = "#event.hostId()")
+    //    @RedissonLock(value = "#event.hostId()")
     public void visitProfile(RedisDto.Visit event) {
-        RedisVisitCount redisVisitCount = findVisitCountByHostId(event.hostId());
-        redisVisitCount.visit();
-        redisVisitCountRepository.save(redisVisitCount);
+//        RedisVisitCount redisVisitCount = findVisitCountByHostId(event.hostId());
+        visit(event.hostId());
+//        redisVisitCount.visit();
+//        redisVisitCountRepository.save(redisVisitCount);
     }
 
     public RedisVisitCount findVisitCountByHostId(Long hostId) {
         return redisVisitCountRepository.findById(hostId)
-                .orElseGet(() -> {
-                    RedisCommand.Visited command = profileVisitReadService.findVisitCountById(hostId);
-                    return command.toRedisEntity();
-                });
+            .orElseGet(() -> {
+                RedisCommand.Visited command = profileVisitReadService.findVisitCountById(hostId);
+                return command.toRedisEntity();
+            });
     }
 
     public List<RedisVisitCount> findAllVisitCounts() {
@@ -56,20 +62,22 @@ public class RedisVisitService {
     @RedissonLock(value = "#hostId + #visitorIp")
     public boolean checkVisited(Long hostId, String visitorIp) {
         String id = RedisUtil.generateVisitorId(hostId, visitorIp);
+        boolean exist = redisVisitorRepository.existsById(id);
         // 방문자 로그 기록
         saveVisitor(hostId, visitorIp);
-        return redisVisitorRepository.existsById(id);
+
+        return exist;
     }
 
     public void saveVisitor(Long hostId, String visitorIp) {
         String id = RedisUtil.generateVisitorId(hostId, visitorIp);
 
         RedisVisitor redisVisitor = RedisVisitor.builder()
-                .id(id)
-                .hostId(hostId)
-                .visitorIp(visitorIp)
-                .visitTime(LocalDateTime.now())
-                .build();
+            .id(id)
+            .hostId(hostId)
+            .visitorIp(visitorIp)
+            .visitTime(LocalDateTime.now())
+            .build();
         redisVisitorRepository.save(redisVisitor);
     }
 
@@ -82,5 +90,12 @@ public class RedisVisitService {
     public void deleteAllVisitors(List<RedisVisitor> visitors) {
         List<String> ids = visitors.stream().map(RedisVisitor::getId).toList();
         redisVisitorRepository.deleteAllById(ids);
+    }
+
+    private void visit(Long hostId) {
+        RMap<String, Integer> visitMap = redissonClient.getMap(
+            RedisConstants.VISIT_COUNT_KEY + ":" + hostId, StringCodec.INSTANCE);
+        visitMap.addAndGet(RedisConstants.DAILY_VISIT_COUNT_FIELD, 1);
+        visitMap.addAndGet(RedisConstants.TOTAL_VISIT_COUNT_FIELD, 1);
     }
 }
