1b469172d5b0c5a2ced3064df4087fe4c0238ca8
hjinshin <gudwls818@gmail.com>
2025-03-31 11:35:11 +0900

    Fix: 데드락 해결
    Ranking과 Answer 엔티티에서 Users 외래키 제거

--- src/main/java/supernova/whokie/answer/infrastructure/repository/AnswerRepository.java
+++ src/main/java/supernova/whokie/answer/infrastructure/repository/AnswerRepository.java
@@ -2,12 +2,10 @@ package supernova.whokie.answer.infrastructure.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import supernova.whokie.answer.Answer;
-import supernova.whokie.user.Users;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,12 +17,17 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
 //    Page<Answer> findAllByPickedAndCreatedAtBetween(Pageable pageable, @Param("user") Users user,
 //        @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
-    @Query("SELECT a FROM Answer a JOIN FETCH a.question q WHERE a.picked = :user AND a.createdAt BETWEEN :startDate AND :endDate ORDER BY a.createdAt DESC")
-    Page<Answer> findAllByPickedAndCreatedAtBetween(Pageable pageable, @Param("user") Users user,
-        @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+//    @Query("SELECT a FROM Answer a JOIN FETCH a.question q WHERE a.picked = :user AND a.createdAt BETWEEN :startDate AND :endDate ORDER BY a.createdAt DESC")
+//    Page<Answer> findAllByPickedAndCreatedAtBetween(Pageable pageable, @Param("user") Users user,
+//        @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
+//
+//    @Query("SELECT DISTINCT FUNCTION('day', p.createdAt) FROM Answer p WHERE p.picked = :user AND p.createdAt BETWEEN :startDate AND :endDate")
+//    List<Integer> findDistinctDaysWithCreatedAtBetween(@Param("user") Users user,
+//        @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
-    @Query("SELECT DISTINCT FUNCTION('day', p.createdAt) FROM Answer p WHERE p.picked = :user AND p.createdAt BETWEEN :startDate AND :endDate")
-    List<Integer> findDistinctDaysWithCreatedAtBetween(@Param("user") Users user,
-        @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+    Page<Answer> findByPickedIdAndCreatedAtBetweenOrderByCreatedAtDesc(Pageable pageable, Long pickedId, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query("SELECT DISTINCT FUNCTION('day', p.createdAt) FROM Answer p WHERE p.pickedId = :userId AND p.createdAt BETWEEN :startDate AND :endDate")
+    List<Integer> findDistinctDaysWithCreatedAtBetween(@Param("userId") Long userId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
