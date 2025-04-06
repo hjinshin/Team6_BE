29c75a0ee2718c28bd0ec3e1860ba4f4c90555f7
momnpa333 <momnpa333@gmail.com>
2025-04-06 19:03:36 +0900

    test: redissonClient 주입

--- src/test/java/supernova/whokie/answer/AnswerIntegrationTest.java
+++ src/test/java/supernova/whokie/answer/AnswerIntegrationTest.java
@@ -41,11 +41,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource(properties = {
-        "jwt.secret=abcd",
-        "url.secret-key=abcd",
-        "spring.sql.init.mode=never"
+    "jwt.secret=abcd",
+    "url.secret-key=abcd",
+    "spring.sql.init.mode=never"
 })
-@MockBean({S3Client.class, S3Template.class, S3Presigner.class, RedissonClient.class})
+@MockBean({S3Client.class, S3Template.class, S3Presigner.class})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class AnswerIntegrationTest {
 
@@ -61,13 +61,15 @@ class AnswerIntegrationTest {
     private AnswerRepository answerRepository;
     @Autowired
     private GroupRepository groupRepository;
+    @Autowired
+    private RedissonClient redissonClient;
 
     @BeforeEach
     void setUp() {
+        redissonClient.getKeys().flushall();
         Users user = createUser(0);
 
         createGroup(0);
-
 
         for (int i = 1; i <= 5; i++) {
             createFriendUser(i);
@@ -94,15 +96,15 @@ class AnswerIntegrationTest {
         request.setAttribute("userId", "1");
 
         mockMvc.perform(get("/api/answer/refresh")
-                        .requestAttr("userId", "1")
-                        .requestAttr("role", "USER")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.users").isArray())
-                .andDo(result -> {
-                    String responseContent = result.getResponse().getContentAsString();
-                    System.out.println("users 내용: " + responseContent);
-                });
+                .requestAttr("userId", "1")
+                .requestAttr("role", "USER")
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.users").isArray())
+            .andDo(result -> {
+                String responseContent = result.getResponse().getContentAsString();
+                System.out.println("users 내용: " + responseContent);
+            });
 
     }
 
@@ -119,15 +121,15 @@ class AnswerIntegrationTest {
         Long pickedId = 2L;
 
         String requestBody = String.format("{\"questionId\": %d, \"pickedId\": %d}", questionId,
-                pickedId);
+            pickedId);
 
         mockMvc.perform(post("/api/answer/common")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody)
-                        .requestAttr("userId", "1")
-                        .requestAttr("role", "USER"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value("답변 완료"));
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .requestAttr("userId", "1")
+                .requestAttr("role", "USER"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.message").value("답변 완료"));
         Users userAfterAnswer = userRepository.findById(userId).orElseThrow();
         int finalPoint = userAfterAnswer.getPoint();
         assertThat(finalPoint).isEqualTo(initialPoint + AnswerConstants.ANSWER_POINT);
@@ -141,7 +143,8 @@ class AnswerIntegrationTest {
 
             Question question = createQuestion(i, userRepository.findById(1L).orElseThrow());
 
-            createAnswer(question, userRepository.findById(1L).orElseThrow(), userRepository.findById(2L).orElseThrow());
+            createAnswer(question, userRepository.findById(1L).orElseThrow(),
+                userRepository.findById(2L).orElseThrow());
         }
 
         MockHttpServletRequest request = new MockHttpServletRequest();
@@ -150,23 +153,23 @@ class AnswerIntegrationTest {
         String currentDate = LocalDate.now().toString();
 
         mockMvc.perform(get("/api/answer/record")
-                        .requestAttr("userId", "1")
-                        .requestAttr("role", "USER")
-                        .param("page", "0")
-                        .param("size", "10")
-                        .param("date",currentDate)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").isArray())
-                .andExpect(jsonPath("$.content.length()").value(5))
-                .andExpect(jsonPath("$.page").value(0))
-                .andExpect(jsonPath("$.size").value(10))
-                .andExpect(jsonPath("$.totalPages").value(1))
-                .andExpect(jsonPath("$.totalElements").value(5))
-                .andDo(result -> {
-                    String responseContent = result.getResponse().getContentAsString();
-                    System.out.println("전체 질문 기록 내용: " + responseContent);
-                });
+                .requestAttr("userId", "1")
+                .requestAttr("role", "USER")
+                .param("page", "0")
+                .param("size", "10")
+                .param("date", currentDate)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.content").isArray())
+            .andExpect(jsonPath("$.content.length()").value(5))
+            .andExpect(jsonPath("$.page").value(0))
+            .andExpect(jsonPath("$.size").value(10))
+            .andExpect(jsonPath("$.totalPages").value(1))
+            .andExpect(jsonPath("$.totalElements").value(5))
+            .andDo(result -> {
+                String responseContent = result.getResponse().getContentAsString();
+                System.out.println("전체 질문 기록 내용: " + responseContent);
+            });
     }
 
     @Test
@@ -175,19 +178,19 @@ class AnswerIntegrationTest {
         String answerId = "1";
 
         mockMvc.perform(get("/api/answer/hint/{answer-id}", answerId)
-                        .requestAttr("userId", "1")
-                        .requestAttr("role", "USER")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.hints").isArray())
-                .andExpect(jsonPath("$.hints.length()").value(3))
-                .andExpect(jsonPath("$.hints[0].valid").value(true))
-                .andExpect(jsonPath("$.hints[1].valid").value(true))
-                .andExpect(jsonPath("$.hints[2].valid").value(false))
-                .andDo(result -> {
-                    String responseContent = result.getResponse().getContentAsString();
-                    System.out.println("Hints 내용: " + responseContent);
-                });
+                .requestAttr("userId", "1")
+                .requestAttr("role", "USER")
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.hints").isArray())
+            .andExpect(jsonPath("$.hints.length()").value(3))
+            .andExpect(jsonPath("$.hints[0].valid").value(true))
+            .andExpect(jsonPath("$.hints[1].valid").value(true))
+            .andExpect(jsonPath("$.hints[2].valid").value(false))
+            .andDo(result -> {
+                String responseContent = result.getResponse().getContentAsString();
+                System.out.println("Hints 내용: " + responseContent);
+            });
     }
 
     @Test
@@ -202,16 +205,16 @@ class AnswerIntegrationTest {
         String requestBody = String.format("{\"answerId\": %d}", answerId);
 
         mockMvc.perform(post("/api/answer/hint")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody)
-                        .requestAttr("userId", "1")
-                        .requestAttr("role", "USER"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value("힌트를 성공적으로 구매하였습니다!"))
-                .andDo(result -> {
-                    String responseContent = result.getResponse().getContentAsString();
-                    System.out.println("구매 응답 내용: " + responseContent);
-                });
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .requestAttr("userId", "1")
+                .requestAttr("role", "USER"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.message").value("힌트를 성공적으로 구매하였습니다!"))
+            .andDo(result -> {
+                String responseContent = result.getResponse().getContentAsString();
+                System.out.println("구매 응답 내용: " + responseContent);
+            });
 
         //유저 포인트 감소 확인
         Users userAfterPurchase = userRepository.findById(userId).orElseThrow();
@@ -228,20 +231,20 @@ class AnswerIntegrationTest {
         Long pickedId = 2L;
 
         String requestBody = String.format(
-                "{\"questionId\": %d, \"groupId\": %d, \"pickedId\": %d}",
-                questionId, groupId, pickedId
+            "{\"questionId\": %d, \"groupId\": %d, \"pickedId\": %d}",
+            questionId, groupId, pickedId
         );
         mockMvc.perform(post("/api/answer/group")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody)
-                        .requestAttr("userId", "1")
-                        .requestAttr("role", "USER"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value("그룹 질문 답변 완료"))
-                .andDo(result -> {
-                    String responseContent = result.getResponse().getContentAsString();
-                    System.out.println("그룹 질문 답변 내용: " + responseContent);
-                });
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .requestAttr("userId", "1")
+                .requestAttr("role", "USER"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.message").value("그룹 질문 답변 완료"))
+            .andDo(result -> {
+                String responseContent = result.getResponse().getContentAsString();
+                System.out.println("그룹 질문 답변 내용: " + responseContent);
+            });
 
         Users userAfterAnswer = userRepository.findById(userId).orElseThrow();
         int finalPoint = userAfterAnswer.getPoint();
@@ -260,29 +263,30 @@ class AnswerIntegrationTest {
                 .requestAttr("userId", "1")
                 .requestAttr("role", "USER")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.days").isArray())
-                .andExpect(jsonPath("$.days").value(org.hamcrest.Matchers.containsInAnyOrder(todayDay)));
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.days").isArray())
+            .andExpect(
+                jsonPath("$.days").value(org.hamcrest.Matchers.containsInAnyOrder(todayDay)));
 
     }
 
     private void createAnswer(Question question, Users picker, Users picked) {
         Answer answer = Answer.builder()
-                .question(question)
-                .pickerId(picker.getId())
-                .pickedId(picked.getId())
-                .hintCount(2)
-                .build();
+            .question(question)
+            .pickerId(picker.getId())
+            .pickedId(picked.getId())
+            .hintCount(2)
+            .build();
         answerRepository.save(answer);
     }
 
     private Question createQuestion(int index, Users user) {
         Question question = Question.builder()
-                .content("Test Question " + index)
-                .questionStatus(QuestionStatus.APPROVED)
-                .writer(user)
-                .groupId(1L)
-                .build();
+            .content("Test Question " + index)
+            .questionStatus(QuestionStatus.APPROVED)
+            .writer(user)
+            .groupId(1L)
+            .build();
         questionRepository.save(question);
         return question;
     }
@@ -290,47 +294,47 @@ class AnswerIntegrationTest {
     private void setFriendRelation(int index, Users user) {
         Users friendUser = userRepository.findById((long) index).orElseThrow();
         Friend friend = Friend.builder()
-                .hostUser(user)
-                .friendUser(friendUser)
-                .build();
+            .hostUser(user)
+            .friendUser(friendUser)
+            .build();
         friendRepository.save(friend);
     }
 
     private void createFriendUser(int index) {
         Users friendUser = Users.builder()
-                .name("Friend " + index)
-                .email("friend" + index + "@example.com")
-                .point(100)
-                .birthDate(LocalDate.now())
-                .kakaoId(1234567890L + index)
-                .gender(Gender.F)
-                .imageUrl("default_image_url_friend_" + index + ".jpg")
-                .role(Role.USER)
-                .build();
+            .name("Friend " + index)
+            .email("friend" + index + "@example.com")
+            .point(100)
+            .birthDate(LocalDate.now())
+            .kakaoId(1234567890L + index)
+            .gender(Gender.F)
+            .imageUrl("default_image_url_friend_" + index + ".jpg")
+            .role(Role.USER)
+            .build();
 
         userRepository.save(friendUser);
     }
 
     private void createGroup(int index) {
         Groups group = Groups.builder()
-                .groupName("Test Group " + index)
-                .description("Test Group " + index)
-                .groupImageUrl("default_image_url.jpg")
-                .build();
+            .groupName("Test Group " + index)
+            .description("Test Group " + index)
+            .groupImageUrl("default_image_url.jpg")
+            .build();
         groupRepository.save(group);
     }
 
     private Users createUser(int index) {
         Users user = Users.builder()
-                .name("Test User " + index)
-                .email("test@example.com")
-                .point(100)
-                .birthDate(LocalDate.now())
-                .kakaoId(1234567890L)
-                .gender(Gender.M)
-                .imageUrl("default_image_url.jpg")
-                .role(Role.USER)
-                .build();
+            .name("Test User " + index)
+            .email("test@example.com")
+            .point(100)
+            .birthDate(LocalDate.now())
+            .kakaoId(1234567890L)
+            .gender(Gender.M)
+            .imageUrl("default_image_url.jpg")
+            .role(Role.USER)
+            .build();
         userRepository.save(user);
         return user;
     }
